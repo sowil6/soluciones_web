cf9ac45255a1f6cec1a86eb9d2087bbeb8ad7387
sowil6 <44715660+sowil6@users.noreply.github.com>
2018-11-02 20:26:44 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/PlantillaUnaColumna.dwt.php" codeOutsideHTMLIsLocked="false" --><head>
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title> <?php include("titulo.php");?></title>
+<!-- InstanceEndEditable -->
+<!--<link href="../Styles/bootstrapCSS/bootstrap.css" rel="stylesheet" type="text/css">
+<script src="../Scripts/jquery-css-transform.js" type="text/javascript"></script>-->
+ 
+
+
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+
+</head>
+
+<body>
+
+<div class="containerCeficc">
+  <header>
+  
+  <?php include("cabecera.php")?>
+   </header>
+
+  <!-- InstanceBeginEditable name="EditRegionUnaColumna" -->
+  <article class="contentUnaColumna">
+      
+<?php include("includeslide.php")?>
+  
+  </article>
+  <!-- InstanceEndEditable --><!-- end .content -->
+
+  <footer>
+     <?php include("footer.php")?>
+     </footer>
+ </div> <!-- end .container -->
+ 
+</body>
+<!-- InstanceEnd --></html>
