c412d032ed08fd9c3f75485929c9c85947673c62
sowil6 <sowil6@gmail.com>
2018-11-14 18:23:28 -0500

    cam6pm

--- vista/catalogoform.php
+++ vista/catalogoform.php
@@ -25,11 +25,85 @@
   </div>
  
  <!-- InstanceBeginEditable name="EditRegionCentro" -->
-
+<link rel="stylesheet" type="text/css" href="/soluciones-web/Styles/css_Catalogo_Form.css">
   <article class="contentCentro">
-<link href="../Styles/CSSEstiloGeneral.css" rel="stylesheet" type="text/css">
-  
+   
+<!--<link href="../Styles/CSSEstiloGeneral.css" rel="stylesheet" type="text/css">
+-->  
     <div class="contenedorCatalogo">
+    
+    <div class="div_ulOpcion_Menu">
+  <div class="divTitulo_ulOpcion_Menu">
+<p>OFERTA ACADEMICA</p> 
+ </div>
+         <?php
+       
+$master="MenuIzquierdo";
+$canciones = simplexml_load_file("../XMLPage/xmlMenuCatalogo.xml");
+echo'	<ul class="ulOpcion_Menu">';
+foreach($canciones as $cancion)
+{
+	$info = new SplFileInfo($cancion->foto);//obtenemos la extension del archivo
+	$ext= $info->getExtension();
+$estado=ValidaExtension($ext);//con la extension evaluamos si es tipo imagen o video
+if($estado==1){
+	//si es imagen se embebe en el control html imagen
+$HTMLfoto_OVideo= "<embed class='" .$master. "imagenoVideo '   src = '../Img/".$cancion->foto. "' />";
+}else{
+	//si es video se embebe en el control html video
+$HTMLfoto_OVideo= "<video controls  class='" .$master. "imagenoVideo'> <source src = '../Img/".$cancion->foto. "' type = 'video/mp4' > <source src = '../img/" . $cancion->foto. "' type = 'video/ogg' ></ video >";
+		}
+	$c2 = $_GET['Accion'];
+if($cancion->Codigo==$c2){
+echo '<li><span class="titulo">'. $cancion->Titulo.'</span>';
+echo'<div class="ulOpcion_MenuImagen">'.$HTMLfoto_OVideo.'</div> <div class="MenuIzquierdo_Intro" ><p>'. $cancion->introduccionNoticia.'</p></div>';
+echo '</li>';
+}
+
+}
+echo'</ul>';	
+  
+  /*function ValidaExtension($sExtension) {
+
+            $resul;
+            switch ($sExtension)
+            {
+                case "jpg":
+                case "jpeg":
+                case "png":
+                case "gif":
+                case "bmp":
+                case "JPG":
+                case "JPEG":
+                case "PNG":
+                case "GIF":
+                case "BMP":
+               $resul=TRUE;
+				
+                break;
+
+                default:
+                case "mp4":
+                case "avi":
+                case "wmv":
+				$resul=FALSE;
+                    break;
+            }
+return $resul;
+          
+			
+          			
+        }*/
+?>
+	
+    </div>
+
+    
+    
+    
+    
+    
+    
     <?php
        
 $master="ulcatalogo";
@@ -49,7 +123,8 @@ $HTMLfoto_OVideo= "<video controls  class='" .$master. "imagenoVideo'> <source s
 		}
 	$v2 = $_GET['Accion'];
 if($cancion->DetalleCodigo==$v2){
-echo'<li><a href="'.$cancion->urlFile.'?Accion='.$cancion->Codigo.'&dcod='.$cancion->DetalleCodigo.'">'.$HTMLfoto_OVideo.' </br>'. $cancion->Titulo.'</br> '. $cancion->introduccionNoticia.'</a>';
+
+echo'<li><a href="'.$cancion->urlFile.'?Accion='.$cancion->Codigo.'&dcod='.$cancion->DetalleCodigo.'"> <p class="titulo">'. $cancion->Titulo.'</p>'.$HTMLfoto_OVideo.'<p class="Intro">'. $cancion->introduccionNoticia.'</p></a>';
 echo '</li>';
 }}
 echo'</ul>';	  
