0f9a45b5e589f91358e77816332a657b8f14b2f7
sowil6 <sowil6@gmail.com>
2019-03-23 21:40:35 -0500

    Update reporte_inscripcion.php

--- vista/reporte_inscripcion.php
+++ vista/reporte_inscripcion.php
@@ -10,55 +10,27 @@ $ruta_autoload;
 echo "ruta home " .__FILE__ ."--";
 if(file_exists(RUTA_Mpdf.'vendor/autoload.php')) {
 require_once(RUTA_Mpdf.'vendor/autoload.php');echo "existe el file4";
-$html="
-<!DOCTYPE html>
+$html="<!doctype html>
 <html>
 <head>
-  
-  	<title></title>
+<script type='text/javascript'>
+function generapdf(){ //lo activas con un OnClick
+
+	//alert(html);
+	documento_='75482555';
+	id_estudiante_='245';
+location.href='reporte_inscripcion?documento='+ documento_+'&id_estudiante='+id_estudiante_ ;
+}
+</script>
+<meta charset='utf-8'>
+<title>Documento sin título</title>
 </head>
 
+<body>
+<button type='button' class='btn btn-success'  id='btnNuevo' title='' href='javascript:;' 	onClick='generapdf();'>Generar Pdf</button>
 
-<body class='body_reporte'>
-<header>
-<div id='logo' class='logo clearfix'><!--inicio div logo-->
-
-        <img src='./imgSistema/logo2019cefic.jpg'>
-		</div><!--fin div logo-->
-		<div class='titulo_reporte'>
-		
- <div>Corporación de Educativa de Formación Integral del caribe</div>
-		<div>CEFICC</div>
-		
-		</div><!--fin div class titulo_reporte-->
-      	
- 
-		
-		<div class='row'> <!--inicio row1-->
-	<div class='col-sm-6 izq'><!--fin col a-->
-
-		
-	</div><!--fin col a-->
-
-  	<div class='compania der'><!--inicio col b-->
-      
-		
-		<div><span>Email</span> <a href='mailto:secretaria.general@ceficc.edu.co'>secretaria.general@ceficc.edu.co</a></div>
-		<div><span>Telefonos:</span> 6812539 - (+57) 3012807094</div>
-		<div><a href='www.ceficc.edu.co'><span>Sitio web</span>: www.ceficc.edu.co</a></div>
-		<div><span>Fecha de Impresión:</span>".date('d-m-Y H:i:s')."</div>
-		 
-	 </div><!--fin col b-->
-	  
-</div><!--fin row1-->
-	   
-	   
-	   
-<div class='row'> <!--inicio row2-->
-   
-    </div><h1>CONSTANCIA DE INSCRIPCION
-
-    </header>";
+</body>
+</html>";
 
 
 $mpdf = new \Mpdf\Mpdf();
@@ -69,8 +41,8 @@ $mpdf = new \Mpdf\Mpdf();
 	$mpdf->WriteHTML($css4,1);*/
 
 	$mpdf->WriteHTML($html);
-//	$mpdf->Output("inscripcion.pdf",'I');
-	$mpdf->Output($documento.".pdf",'D');//genera el fichero y forza la descarga
+	$mpdf->Output("inscripcion.pdf",'I');
+//	$mpdf->Output($documento.".pdf",'D');//genera el fichero y forza la descarga
 }else{echo "no existe el archivo";}
 ?>
 
