38cdbce9ca248f59168b07d97f3d0bb26b2e217c
sowil6 <sowil6@gmail.com>
2019-03-23 20:21:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 ///definir ruta
 session_start();
  define("RUTA_BASE", dirname(realpath(__FILE__))."/");
-//echo "en index.php " .RUTA_BASE;
+echo "en index.php " .RUTA_BASE;
 	
 	include "libreria/core.php";
 
