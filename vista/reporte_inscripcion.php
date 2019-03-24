5f9a76671aa3fca3a9bff855968218158fd956ce
sowil6 <sowil6@gmail.com>
2019-03-23 23:20:05 -0500

    Update reporte_inscripcion.php

--- vista/reporte_inscripcion.php
+++ vista/reporte_inscripcion.php
@@ -116,7 +116,8 @@ $mpdf = new \Mpdf\Mpdf();
 	$mpdf->WriteHTML($css4,1);*/
 
 	$mpdf->WriteHTML($html2);
-	$mpdf->Output("inscripcion.pdf",'I');
+	$mpdf->Output();
+//	$mpdf->Output("inscripcion.pdf",'I');
 ////	$mpdf->Output($documento.".pdf",'D');genera el fichero y forza la descarga
 }else{echo "no existe el archivo";}
 ?>
