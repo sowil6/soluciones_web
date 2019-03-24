29984c16e5aa89dc8144f0c28216386e1a2bc16a
sowil6 <sowil6@gmail.com>
2019-03-23 22:15:21 -0500

    Update reporte_inscripcion.php

--- vista/reporte_inscripcion.php
+++ vista/reporte_inscripcion.php
@@ -4,7 +4,7 @@
 //echo "ruta  ".RUTA_BASE."-- -".RUTA_Mpdf;
 echo "ruta home " .__FILE__ ."--";
 if(file_exists(RUTA_Mpdf.'vendor/autoload.php')) {
-require_once(RUTA_Mpdf.'vendor/autoload.php');echo "existe el file3";
+require_once(RUTA_Mpdf.'vendor/autoload.php');echo "existe el file4";
 echo $html="<!doctype html>
 <html>
 <head>
