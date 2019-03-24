61d9f921303ad56f76099ba1e06d0cf3c76da543
sowil6 <sowil6@gmail.com>
2019-03-23 23:09:22 -0500

    Update reporte_inscripcion.php

--- vista/reporte_inscripcion.php
+++ vista/reporte_inscripcion.php
@@ -9,13 +9,13 @@ function Header()
     // Logo
     //$this->Image('./imgSistema/logo2019cefic.jpg',10,8,13);
     // Arial bold 15
- /*   $this->SetFont('Arial','B',15);
+    $this->SetFont('Arial','B',15);
     // Movernos a la derecha
     $this->Cell(50);
     // Título
     $this->Cell(30,10,'No veo el titulo',1,0,'C');
     // Salto de línea
-    $this->Ln(20);*/
+    $this->Ln(20);/**/
 }
 
 // Pie de página
